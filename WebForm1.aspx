fe692d80bd34f6e9a99d4e5fcbb05266af96c5e0
SaniyaMansuri12 <114173445+SaniyaMansuri12@users.noreply.github.com>
2023-03-31 09:18:33 +0530

    Add files via upload

---
+++ WebForm1.aspx
@@ -0,0 +1,74 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="CrudOperationWithStoredProcedureGrid.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style>
+        .Heading{
+            text-align : left;
+        }
+        .auto-style1 {
+            margin-left: 69px;
+        }
+        .auto-style2 {
+            margin-left: 129px;
+        }
+        .auto-style3 {
+            margin-left: 32px;
+        }
+        .auto-style4 {
+            width: 29%;
+            height: 21px;
+        }
+        .auto-style5 {
+            width: 463px;
+            height: 22px;
+        }
+        .auto-style6 {
+            width: 566px;
+            height: 22px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1 class="Heading">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Product Details </h1>
+            <table class="auto-style4">
+                <tr>
+                    <td class="auto-style5">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Select Product&nbsp;</td>
+                    <td class="auto-style6">
+                <asp:DropDownList ID="DropDownList2" runat="server" CssClass="auto-style3" Height="34px" Width="150px">
+                </asp:DropDownList>
+                    </td>
+                </tr>
+            </table>
+            <p class="Heading">
+                <asp:Button ID="btnSearch" runat="server" CssClass="auto-style2" Height="34px" OnClick="btnSearch_Click" Text="Search" Width="99px" />
+            </p>
+            <p class="Heading" style="font-weight: bold; color: #FF0000">
+                &nbsp;&nbsp;&nbsp; NOTE - User Can Only Edit Product Details</p>
+        </div>
+        <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" AutoGenerateEditButton="True" BackColor="White" BorderColor="White" BorderStyle="Ridge" BorderWidth="2px" CellPadding="3" CellSpacing="1" CssClass="auto-style1" GridLines="None" Height="228px" OnPageIndexChanging="GridView1_PageIndexChanging" OnRowCancelingEdit="GridView1_RowCancelingEdit" OnRowEditing="GridView1_RowEditing" OnRowUpdating="GridView1_RowUpdating" Width="762px">
+            <Columns>
+                <asp:BoundField DataField="ProductId" HeaderText="ProductId" />
+                <asp:BoundField DataField="ProductName" HeaderText="ProductName" />
+                <asp:BoundField DataField="ProductDescription" HeaderText="ProductDescription" />
+                <asp:BoundField DataField="CategoryName" HeaderText="CategoryName" />
+                <asp:BoundField DataField="Username" HeaderText="Username" />
+            </Columns>
+            <FooterStyle BackColor="#C6C3C6" ForeColor="Black" />
+            <HeaderStyle BackColor="#4A3C8C" Font-Bold="True" ForeColor="#E7E7FF" />
+            <PagerStyle BackColor="#C6C3C6" ForeColor="Black" HorizontalAlign="Right" />
+            <RowStyle BackColor="#DEDFDE" ForeColor="Black" />
+            <SelectedRowStyle BackColor="#9471DE" Font-Bold="True" ForeColor="White" />
+            <SortedAscendingCellStyle BackColor="#F1F1F1" />
+            <SortedAscendingHeaderStyle BackColor="#594B9C" />
+            <SortedDescendingCellStyle BackColor="#CAC9C9" />
+            <SortedDescendingHeaderStyle BackColor="#33276A" />
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CrudOperationWithStoredProcedureGrid
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Page.IsPostBack == false)
+            {
+                BindGrid();
+            }
+
+        }
+
+        public void BindGrid()
+        {
+            string cs = ConfigurationManager.ConnectionStrings["dbCon"].ConnectionString;
+            SqlConnection con;
+
+            try
+            {
+                using (con = new SqlConnection(cs))
+                {
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand("GetData", con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                    DataSet ds = new DataSet();
+                    sda.Fill(ds);
+                    GridView1.DataSource = ds;
+                    GridView1.DataBind();
+                    DropDownList2.Items.Insert(0, new ListItem("----Select Product----"));
+                    int totatrow = ds.Tables[0].Rows.Count;
+                    int c = 0;
+                    while (c < totatrow)
+                    {
+                        DropDownList2.Items.Add(ds.Tables[0].Rows[c]["ProductName"].ToString());
+                        c++;
+                    }
+                    
+                    con.Close();
+
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        protected void GridView1_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+
+        }
+
+        protected void GridView1_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            GridView1.EditIndex = -1;
+            BindGrid();
+        }
+
+        protected void GridView1_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            GridView1.EditIndex = e.NewEditIndex;
+            BindGrid();
+
+        }
+
+        protected void GridView1_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            string cs = ConfigurationManager.ConnectionStrings["dbCon"].ConnectionString;
+            SqlConnection con;
+
+            string ProductId, ProductName, ProductDescription;
+            ProductId = ((TextBox)GridView1.Rows[e.RowIndex].Cells[1].Controls[0]).Text;
+            ProductName = ((TextBox)GridView1.Rows[e.RowIndex].Cells[2].Controls[0]).Text;
+            ProductDescription = ((TextBox)GridView1.Rows[e.RowIndex].Cells[3].Controls[0]).Text;
+
+            
+            string s = "Update SP_Update set ProductName = '" + ProductName + "', ProductDescription= '" + ProductDescription + "' where ProductId = '" + ProductId + "'";
+            using (con = new SqlConnection(cs))
+            {
+                SqlCommand cmd = new SqlCommand("SP_Update", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "SP_Update";
+                cmd.Parameters.AddWithValue("@ProductId", ProductId);
+                cmd.Parameters.AddWithValue("@ProductName", ProductName);
+                cmd.Parameters.AddWithValue("@ProductDescription", ProductDescription);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                GridView1.EditIndex = -1;
+                BindGrid();
+                con.Close();
+            }                
+        }
+
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            string cs = ConfigurationManager.ConnectionStrings["dbCon"].ConnectionString;
+            SqlConnection con;
+            string s = "select * from Search_SP where ProductName = " + DropDownList2.SelectedItem.Text;
+            using (con = new SqlConnection(cs))
+            {
+                SqlCommand cmd = new SqlCommand("Search_SP", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "GetData";
+                con.Open();
+                cmd.ExecuteNonQuery();
+                SqlDataAdapter sda = new SqlDataAdapter();
+                sda.SelectCommand = cmd;
+                DataSet ds = new DataSet();
+                sda.Fill(ds);
+                GridView1.DataSource = ds;
+                GridView1.DataBind();
+                
+                con.Close();
+            }         
+        }
+    }
+}
+    
